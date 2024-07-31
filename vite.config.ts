a34f2b70703a5fdaf9df425c9a9c617f9ea4de10
Mallikarjuna reddy <mallikarjunareddy8890@gmail.com>
2024-07-31 13:45:42 +0530

    Update base path and deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base:'Mobiscroll_monthly_calendar',
+  base:'/Mobiscroll_monthly_calendar',
   plugins: [react()],
   server: {
     port: 3000,
